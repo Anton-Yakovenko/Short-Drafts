891f8343a67a97c49443b4c9ae6bca1db614dc33
Anton <over.uran@gmail.com>
2021-03-12 19:43:07 +0200

    C code

---
+++ c_start/start.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    int x = 14, y = 8, res, user;
+    scanf("%d", &user);
+
+    res = user + x + y; 
+
+    printf ("Res = %d\n", res);
+    printf ("Your res is\n", res);
+
+    return 0;
+}
