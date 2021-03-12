9ba7801628b6841ebfb7a80ccba80cd916da2f4c
Anton <over.uran@gmail.com>
2021-03-13 00:28:45 +0200

    calculator V1

--- c_start/start.c
+++ c_start/start.c
@@ -1,30 +1,67 @@
+// calculator 
 #include <stdio.h>
 
-void print (int res)
-{
-    switch (res)
-        {
-            case 1:
-                printf("OK");
-                break;
-            case 2:
-                printf("Not OK");
-                break;
-            
-            default:
-                printf("Default");
-                break;
-        }
-        printf ("\n");
+void input(float *num1, float *num2)
+{   
+    printf("num 1: ");
+    scanf("%f", &*num1);
+
+
+    printf("num 2: ");
+    scanf("%f", &*num2);
 }
+
 
 int main()
 {
-    int res;
-    for (int i = 0; i < 3; i++)
+    char choice;
+    float num1, num2, res;
+
+    printf("Welcome! \nPlease choose option: sum(S), difference(D), multiplication(M), division(V)\nS/D/M/V: ");
+    scanf("%c", &choice);
+
+    input(&num1, &num2);
+
+    if (choice == 'S')
+        res = num1 + num2;
+
+    else if (choice == 'D')  
+        res = num1 - num2;
+
+    else if (choice == 'M')
+        res = num1 * num2;
+    
+    else if (choice = 'V')
+        res = num1 / num2;
+
+    else
     {
-        scanf ("%d", &res);  
-        print (res);  
+        res = 0;
+        printf("unknown choice - %c", choice);
     }
+
+    printf("res = %.2f\n", res);
+
     return 0;
 }
+
+
+
+
+// void print (int res)
+// {
+//     switch (res)
+//         {
+//             case 1:
+//                 printf("OK");
+//                 break;
+//             case 2:
+//                 printf("Not OK");
+//                 break;
+            
+//             default:
+//                 printf("Default");
+//                 break;
+//         }
+//         printf ("\n");
+// }
